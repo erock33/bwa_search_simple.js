3b1a73aef1a404886ebdad6ea5eff453d98b3c6e
erich <eric.hobbs33@gmail.com>
2014-12-02 16:42:51 -0800

    Add ranks to index object and got findLocations function working

--- bwa_search_simple.js
+++ bwa_search_simple.js
@@ -24,6 +24,7 @@ function createBWIndex(text){
     index.bwtarray = [];
     index.c = [];
     index.c_ord = {};
+    index.ranks = [];
     for(var i=0; i < index.suffixarray.length; i++){
 
         // Construct BWT array
@@ -44,6 +45,20 @@ function createBWIndex(text){
             index.c.push(i);
             index.c_ord[f_char] = index.c.length - 1;
         }
+
+        var l_char = index.txt.charAt(index.bwtarray[
+            index.bwtarray.length - 1]);
+        var rank = {};
+        if( i > 0 ){
+            rank = Object.create(
+                index.ranks[ index.ranks.length -1]);
+        } 
+        if(l_char in rank){
+            rank[l_char]++;
+        } else {
+            rank[l_char] = 1;
+        }
+        index.ranks.push(rank);
     }
 
     return index;
@@ -60,5 +75,31 @@ function createBWIndex(text){
  *     appears in the text
  */
 function findLocations(index, str){
+    var start = null;
+    var end = null;
+    var rank_start = null;
+    var rank_end = null;
 
+
+    var chr = str.substring(str.length - 1, str.length);
+    var chr_ord = index.c_ord[chr];
+    start = index.c[chr_ord];
+    end = (chr_ord == index.c.length - 1) ?
+        index.bwtarray.length-1 : index.c[ chr_ord+1 ];
+
+    for(var i=0; i < str.length -1; i++){
+        var next_chr = str.substring(str.length-i-2, 
+            str.length-i-1);
+        rank_start = (isNaN(index.ranks[start][next_chr])) ? 
+            1 : index.ranks[start][next_chr];
+
+        rank_end = (isNaN(index.ranks[end][next_chr])) ?
+            1 : index.ranks[end][next_chr];
+
+        start = index.c[index.c_ord[next_chr]] + 
+            rank_start - 1;
+        end = index.c[ index.c_ord[next_chr]] +
+            rank_end - 1;
+    }
+    return [start, end];
 }
